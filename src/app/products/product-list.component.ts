5b35a6d629d9cc846a1a7eb131ac7564f2375ee7
Abd-Allah Eid <abdallah.mohamedfcih@gmail.com>
2019-06-13 22:50:51 +0200

    add filter products feature and a pipe that replace any special character with space

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,26 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {IProduct} from './product'
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    _listFilter: string;
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+       this._listFilter = value;
+       this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         
   {
@@ -36,7 +45,22 @@ export class ProductListComponent {
   },
 
   ]
+  constructor() {
+    this.listFilter = 'cart';
+    this.filteredProducts = this.products;
+  }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
+
   toggleImage():void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit():void {
+    console.log("Hello inside OnInit")
   }
 }
---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,10 @@
+import { PipeTransform, Pipe } from "@angular/core";
+@Pipe({
+    name:'convertToSpaces'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform {
+    transform(value: string, character: string): string {
+        return value.replace(character,' ');
+    }
+}
