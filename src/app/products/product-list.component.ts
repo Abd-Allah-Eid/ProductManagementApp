90f2869e744c21bb9f628e41d52d90162b6aa54a
Abd-Allah Eid <abdallah.mohamedfcih@gmail.com>
2019-06-17 21:40:41 +0200

    adding static star rating based on dummy data, implement an angulr service for products

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {IProduct} from './product'
+import { ProductService} from './product.service'
 
 @Component({
     selector: 'pm-products',
@@ -21,33 +22,9 @@ export class ProductListComponent implements OnInit {
        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        
-  {
-    "productId": 1,
-    "productName": "Leaf Rake",
-    "productCode": "GDN-0011",
-    "releaseDate": "March 19, 2016",
-    "description": "Leaf rake with 48-inch wooden handle.",
-    "price": 19.95,
-    "starRating": 3.2,
-    "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-  },
-  {
-    "productId": 2,
-    "productName": "Garden Cart",
-    "productCode": "GDN-0023",
-    "releaseDate": "March 18, 2016",
-    "description": "15 gallon capacity rolling garden cart",
-    "price": 32.99,
-    "starRating": 4.2,
-    "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-  },
-
-  ]
-  constructor() {
-    this.listFilter = 'cart';
-    this.filteredProducts = this.products;
+    products: IProduct[] = []
+  constructor(private productService: ProductService) {
+    // this.listFilter = 'cart';
   }
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
@@ -61,6 +38,11 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit():void {
-    console.log("Hello inside OnInit")
+    this.products = this.productService.getProducts()
+    this.filteredProducts = this.products;
+  }
+
+  onNotify(message: number): void{
+    this.pageTitle = 'Product List ' + message
   }
 }
