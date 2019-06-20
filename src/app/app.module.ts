63a52e241633c124d3e6416ca0f5f4b2593e24b8
Abd-Allah Eid <abdallah.mohamedfcih@gmail.com>
2019-06-20 17:26:25 +0200

    adding some basic routing through the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,30 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe'
 import {StarComponent} from './shared/star.component'
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component'
+import { WelcomeComponent } from './home/welcome.component'
+import { RouterModule } from '@angular/router'
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
