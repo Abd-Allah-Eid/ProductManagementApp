f915e25754436f7a677b08fade2f1c47665a7eef
Abd-Allah Eid <abdallah.mohamedfcih@gmail.com>
2019-06-21 01:36:25 +0200

    add structre and style to product detail template

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ActivatedRoute, Router} from '@angular/router'
+import { ProductService} from './product.service'
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,22 +10,26 @@ import { ActivatedRoute, Router} from '@angular/router'
 export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = "Product Detail";
-  product : IProduct;
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  products: IProduct[] = [];
+  myProduct: object;
+  errorMessage: string; 
+  constructor(private route: ActivatedRoute, private router: Router,private productService: ProductService) { }
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product = {
-      "productId": id,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2015",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-    }
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products
+        for(let i = 0; i < this.products.length; i++) {
+          if(this.products[i].productId === id) {
+            return this.myProduct = this.products[i];
+          }
+        }
+      },
+      error => this.errorMessage = <any>error
+    )
+    
+    
   }
 
   onBack():void {
